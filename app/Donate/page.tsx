75adf832c2591420f910a2e5fc103a9a8f75149e
Charlene <ckkim517@gmail.com>
2025-05-02 16:12:18 -0700

    Committing changes to install jest for testing

--- app/Donate/page.tsx
+++ app/Donate/page.tsx
@@ -1,5 +1,28 @@
 export default function Donate() {
     return (
-        <>This is donate.</>
+        <main className="p-8 max-w-3xl mx-auto">
+            <h1 className="text-4xl font-bold text-center mb-8">
+                LYFF Champions - Donate Today
+            </h1>
+
+            <section className="mb-6">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                    With your generous support, we can make a difference in the lives of felines in need.
+                </h2>
+                <p className="text-gray-700 leading relaxed">
+                    Love Your Feral Felines is a registered 501(c)3 tax exempt organization. All donations are tax deductible!
+                </p>
+                <p className="text-gray-700 leading relaxed">
+                    Tax ID: 46-4415187
+                </p>
+            </section>
+
+            <section className="mb-6">
+                <h3 className="text-2xl font-semibold text-gray-800 mb-2">
+                    Donate Online
+                </h3>
+            </section>
+
+        </main>
     )
 }
